0f683a365ac4b5dbc2409c98c0026870471ad876
thomas-gribling <tgriblin@student.42perpignan.fr>
2025-01-13 14:59:01 +0100

    dockerfiles should be good

---
+++ srcs/requirements/nginx/tools/script.sh
@@ -0,0 +1,28 @@
+ >#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $TLS_KEY -out $TLS_CERT -subj "/C=FR/L=PO/O=42/OU=student/CN=$WP_URL"
+
+echo "
+server {
+	listen 443 ssl;
+	listeb [::]:443 ssl;
+
+	server name www.$WP_URL $WP_URL;
+
+	ssl_certificate $TLS_CERT;
+	ssl_certificate_key $TLS_KEY;
+
+	ssl_protocols TLSv1.3;
+
+	index index.php;
+	root /var/www/html;
+
+	location ~ [^/]\.php(/|$) {
+		try_files $uri = 404;
+		fastcgi_pass wordpress:9000;
+		include fastcgi_params;
+		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+	}
+} " > /etc/nginx/sites-available/default
+
+nginx -g "daemon off;"
