35dbdfc28f063496903afedee32a59a403be4f32
thomas-gribling <tgriblin@student.42perpignan.fr>
2025-01-20 16:46:24 +0100

    trying to fix

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+service mariadb start
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost'  ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
+
+kill $(cat /run/mysqld/mysqld.pid)
+
+mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir /var/www
-mkdir /var/www/html
+mkdir -p /var/www
+mkdir -p /var/www/html
 
 cd /var/www/html
 
@@ -20,17 +20,13 @@ mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php # // line 23
 sed -i -r "s/username_here/$DB_USER/1" wp-config.php # // line 26
 sed -i -r "s/password_here/$DB_PASS/1" wp-config.php # // line 29
-sed -i -r "s/localhost/mariadb/1" wp-config.php # // line 32
+#sed -i -r "s/localhost/mariadb/1" wp-config.php # // line 32
 
 wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_enail=$WP_ADMIN_MAIL --skip-email --allow-root
 wp user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS --allow-root
 
-#wp theme install redis-cache --activate --allow-root
-
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
-mkdir /run/php
-
-#wp redis enable --allow-root
+mkdir -p /run/php
 
 /usr/sbin/php-fpm7.4 -F
