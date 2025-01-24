c30fdf6a023c09bea0f2e6101c9b457a43ff9917
thomas-gribling <tgriblin@student.42perpignan.fr>
2025-01-24 17:02:11 +0100

    not working but also not not working

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS' ;" >> db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
 
-mysqld < db1.sql
+#mysqld < db.sql
+
+service mariadb start
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysqladmin -u root -p$DB_ROOT_PASS shutdown
