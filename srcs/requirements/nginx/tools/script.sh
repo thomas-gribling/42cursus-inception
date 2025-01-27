478b12ad5931cfa41e30bf18578dcc8fc3a80fb1
thomas-gribling <tgriblin@student.42perpignan.fr>
2025-01-27 10:38:29 +0100

    changed nginx

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -2,35 +2,8 @@
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $TLS_KEY -out $TLS_CERT -subj "/C=FR/L=PO/O=42perpignan/OU=student/CN=$WP_URL"
 
-echo "
-server {
-	listen 443 ssl;
-	listen [::]:443 ssl;
+sed -i -r "s/WP_URL/$WP_URL/g" /etc/nginx/sites-available/default
 
-	server_name www.$WP_URL $WP_URL;
-
-	ssl_certificate $TLS_CERT;
-	ssl_certificate_key $TLS_KEY;
-
-	ssl_protocols TLSv1.3;
-
-	index index.php;
-	root /var/www/html;" > /etc/nginx/sites-available/default
-
-echo '
-	location / {
-		try_files \$uri \$uri/ /index.php?\$args;
-	}
-
-	location ~ [^/]\.php(/|$) {
-		try_files $uri = 404;
-		fastcgi_pass wordpress:9000;
-		include fastcgi_params;
-		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-	}
-
-	error_page 404 /404.html;
-	error_page 500 502 503 504 /50x.html;
-} ' >> /etc/nginx/sites-available/default
+echo "127.0.0.1   www.$WP_URL  $WP_URL" >> /etc/hosts
 
 nginx -g "daemon off;"
